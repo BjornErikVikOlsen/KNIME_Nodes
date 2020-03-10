fd53f89d500ef15a397b9416f551ff27efe7b7a2
BjornErikVikOlsen <94bevo@gmail.com>
2020-03-10 12:02:11 +0100

    Init commit

---
+++ src/src/KNIME_URL_Extract.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class KNIME_URL_Extract {
+
+    /*Pattern pattern = Pattern.compile(
+            "\\b(((ht|f)tp(s?)\\:\\/\\/|~\\/|\\/)|www.)" +
+                    "(\\w+:\\w+@)?(([-\\w]+\\.)+(com|org|net|gov" +
+                    "|mil|biz|info|mobi|name|aero|jobs|museum" +
+                    "|travel|[a-z]{2}))(:[\\d]{1,5})?" +
+                    "(((\\/([-\\w~!$+|.,=]|%[a-f\\d]{2})+)+|\\/)+|\\?|#)?" +
+                    "((\\?([-\\w~!$+|.,*:]|%[a-f\\d{2}])+=?" +
+                    "([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)" +
+                    "(&(?:[-\\w~!$+|.,*:]|%[a-f\\d{2}])+=?" +
+                    "([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)*)*" +
+                    "(#([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)?\\b");
+
+    List<String> result = new ArrayList<String>();
+
+    try{
+        Matcher matcher = pattern.matcher(c_Text);
+        while (matcher.find()) {
+            result.add(matcher.group());
+
+        }
+    } catch (
+    PatternSyntaxException ex){
+
+    }
+    out_Text = result.toArray(new String[0]);*/
+}
